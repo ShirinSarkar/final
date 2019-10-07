ece0762cb3bd63348c9fad3d405a51c590f89a80
ShirinSarkar <55285139+ShirinSarkar@users.noreply.github.com>
2019-10-07 09:28:33 +0200

    final-uploud

---
+++ js/script2.js
@@ -0,0 +1,116 @@
+function inClick(){
+
+    document.getElementById("inSection").style = "right:60%"; 
+     
+  }
+  
+ 
+  //  ---------------------------------
+   
+  function theClick(){
+   
+     document.getElementById("theSection").style = "right:50%";
+      
+   }
+  
+   
+     //------------------------------
+     
+    function endClick(){
+     
+       document.getElementById("endSection").style = "right:40%";
+        
+     }
+    
+     //--------7777777777----------- zweite line----------777777777777777777777777---------------------------
+     
+   
+  
+  
+  
+  function iClick(){
+  
+      document.getElementById("iSection").style = "right:70%"; 
+       
+    }
+  
+    //------------------------
+    function wouldClick(){
+     
+        document.getElementById("wouldSection").style = "right:55%";
+        
+     }
+     
+    
+  //-----------------------------------
+  
+      
+       function likeClick(){
+    
+        document.getElementById("likeSection").style = "right:43%"; 
+         
+      }
+  
+  
+    //----------------------------------------------------
+    function toClick(){
+     
+        document.getElementById("toSection").style = "right:35%";
+        
+     }
+     
+  
+    //    ------------------------------------------------------     
+     function thanksClick(){
+    
+        document.getElementById("thanksSection").style = "right:20%"; 
+         
+      }
+     
+     
+  // ----------7777777-----------------dritte line-------77777777777777777777777---------
+    
+  
+  function allClick(){
+     
+      document.getElementById("allSection").style = "right:60%";
+       
+    }
+   
+  
+    //-------------------------------------------------------------------
+   function ofClick(){
+    
+      document.getElementById("ofSection").style = "right:51%";
+       
+    }
+   
+   
+     //--------------------------------------------------------------------------
+     
+     function youClick(){
+      
+         document.getElementById("youSection").style = "right:40%";
+         
+      }
+      
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
+  //  ---------------------------------------
+  
+  
